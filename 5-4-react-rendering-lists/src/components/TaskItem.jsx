96fe12f1afc3deb52f07873bdc95f1ccb28e213b
Lea4040 <s202223660@kfupm.edu.sa>
2026-02-25 13:20:30 +0300

    Task 4 - Complete

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -7,7 +7,9 @@ export default function TaskItem({ task, onToggle, onDelete }) {
       <label className="taskMain">
         {/* TODO (TASK 4): Checkbox calls onToggle(task.id) on change
             Also make it reflect task.isDone using checked={task.isDone} */}
-        <input type="checkbox" />
+        <input type="checkbox" 
+        onChange={() => onToggle(task.id)}
+        checked={task.isDone}/>
 
         {/* TODO (TASK 3): Show <DueBadge /> ONLY when task is NOT done (&&)
             Example:
