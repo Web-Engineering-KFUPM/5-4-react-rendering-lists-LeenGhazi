81e740a8b41edba8992b31301ba6a5cf436178a9
Lea4040 <s202223660@kfupm.edu.sa>
2026-02-25 13:35:02 +0300

    fix errors

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -19,10 +19,11 @@ export default function TaskItem({ task, onToggle, onDelete }) {
         {/* TODO (TASK 2): Display the task title
             Example:
               <span className="title">{task.title}</span> */}
+        <span className="title">{task.title}</span>
       </label>
 
       {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */}
-      <button className="ghost" aria-label="Delete task">
+      <button className="ghost" aria-label="Delete task" onClick={() => onDelete(task.id)}>
         ✕
       </button>
     </li>
